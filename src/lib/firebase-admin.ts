2c64dae832020817ec33cd9b504b4bd650fe0856
Malik Faizan <malikfaizan6050@gmail.com>
2026-08-16 22:40:34 +0500

    fix firebase admin connection

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,23 +2,40 @@ import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 
-const firebaseAdmin =
-  getApps().length === 0
-    ? initializeApp({
-        credential: cert({
-          projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-
-          clientEmail:
-            process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-
-          privateKey:
-            process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
-              /\\n/g,
-              "\n"
-            ),
-        }),
-      })
-    : getApps()[0];
+if (
+    !process.env.FIREBASE_ADMIN_PROJECT_ID ||
+    !process.env.FIREBASE_ADMIN_CLIENT_EMAIL ||
+    !process.env.FIREBASE_ADMIN_PRIVATE_KEY
+) {
+    throw new Error(
+        "Missing Firebase Admin environment variables"
+    );
+}
 
 
-export const db = getFirestore(firebaseAdmin);
+
+const app =
+    getApps().length === 0
+        ? initializeApp({
+
+            credential: cert({
+
+                projectId:
+                process.env.FIREBASE_ADMIN_PROJECT_ID,
+
+                clientEmail:
+                process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+
+                privateKey:
+                process.env.FIREBASE_ADMIN_PRIVATE_KEY
+                .replace(/\\n/g,"\n")
+
+            })
+
+        })
+        :
+        getApps()[0];
+
+
+
+export const db = getFirestore(app);
